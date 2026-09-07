e5117aaea3ff357630cb7f2e25b2b5590942b0fe
Rafael <143661914+rafaelqcc@users.noreply.github.com>
2026-09-07 11:13:18 -0300

    Load and initialize Iris from remote source

---
+++ Iris/Example.lua
@@ -0,0 +1,2 @@
+local Iris = loadstring(game:HttpGet("https://raw.githubusercontent.com/rafaelqcc/Other-scripts/refs/heads/main/Iris/Source.lua"))().Init(game.CoreGui)
+Iris:Connect(Iris.ShowDemoWindow)
